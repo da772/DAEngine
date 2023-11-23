62dc56c114ab08a416355acb7d03b5c0ef63a6d4
dan <dnahle@usc.edu>
2023-11-22 21:07:49 -0800

    Update logger.h

--- DAEngine/src/DAEngine/logger.h
+++ DAEngine/src/DAEngine/logger.h
@@ -14,7 +14,7 @@ namespace da {
 #define LOG_WARN(ELogChannel, msg, ...) da::CLogger::LogWarning(ELogChannel, std::string("[%s] ") + msg __FUNCTION__  , ##__VA_ARGS__)
 #define LOG_ERROR(ELogChannel, msg, ...) da::CLogger::LogError(ELogChannel, std::string("[%s] ") + msg, __FUNCTION__  , ##__VA_ARGS__)
 #define LOG_ASSERT(x, ELogChannel, msg, ...) da::CLogger::LogAssert(x, ELogChannel, std::string("[%s] ") + msg, __FUNCTION__  , ##__VA_ARGS__); if (!x) { ASSERT(x); }
-#define LOG_CALLSTACK(ELogType, ELogChannel) da::CLogger::log(ELogType, ELogChannel, ::std::string("[%s] PRINTING CALLSTACK\n") + ::std::to_string(::std::stacktrace::current()), __FUNCTION__)
+#define LOG_CALLSTACK(ELogType, ELogChannel) da::CLogger::log(ELogType, ELogChannel, ::std::string("[%s] PRINTING CALLSTACK\n") + ::std::to_string(::std::stacktrace::current(2)), __FUNCTION__)
 
 #define __LOGFUNC__(x, y) template <typename ...Args> \
 inline static void x(const ELogChannel& channel, const std::string& message, Args ... args) {\
